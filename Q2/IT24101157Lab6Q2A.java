88a3e4e705de387a1539e21cd9ad1c9b869fb474
IT24101157 <it24101157@my.sliit.lk>
2024-09-03 13:27:35 +0530

    IT24101157Lab6Q2A.java

---
+++ Q2/IT24101157Lab6Q2A.java
@@ -0,0 +1,10 @@
+public class IT24101157Lab6Q2A{
+   public static void main(String[]args){
+     int number=1;
+     while(number<=10){ 
+        
+       System.out.print(number +" ");
+       number++;
+     }
+   }
+}
